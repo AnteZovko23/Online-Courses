6b7247635d9bb4e4359c8f754728757b31a0bf9d
zovkoante23 <antezovko.az@gmail.com>
2020-08-24 18:18:00 +0200

    ReactJS+SpringBoot

---
+++ SpringBoot-ReactJS/react-frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import UserComponent from './Components/UserComponent'
+
+function App() {
+  return (
+    <div className="App">
+      <UserComponent />
+    </div>
+  );
+}
+
+export default App;
